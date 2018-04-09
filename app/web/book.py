213fd473a086d21e5effbad5a1b810bd84114e14
XThundering <XThundering@vip.qq.com>
2018-04-09 17:55:07 +0800

    重构

--- app/web/book.py
+++ app/web/book.py
@@ -1,12 +1,14 @@
 """
  Created by XThundering on 2018/4/4
 """
+import json
+
 from flask import jsonify, request
 
 from app.forms.book import SearchForm
 from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
-from app.view_models.book import BookViewModel
+from app.view_models.book import BookCollection
 from . import web
 
 __author__ = 'XThundering'
@@ -20,16 +22,20 @@ def search():
         ?q=金庸&page=1
     """
     form = SearchForm(request.args)
+    books = BookCollection()
+
     if form.validate():
         q = form.q.data.strip()
         page = form.page.data
         isbn_or_key = is_isbn_or_key(q)
+        yushu_book = YuShuBook()
+
         if isbn_or_key == 'isbn':
-            result = YuShuBook.search_by_isbn(q)
-            result = BookViewModel.package_single(result, q)
+            yushu_book.search_by_isbn(q)
         else:
-            result = YuShuBook.search_by_keyword(q, page)
-            result = BookViewModel.package_collection(result, q)
-        return jsonify(result)
+            yushu_book.search_by_keyword(q, page)
+
+        books.fill(yushu_book, q)
+        return json.dumps(books, default=lambda o: o.__dict__)
     else:
         return jsonify(form.errors)
